c8113045d9ef735b18a11b58b2099ac56049873c
Jacq <jacqkirkman@gmail.com>
2023-08-09 19:48:19 -0700

    progress

--- client/src/App.js
+++ client/src/App.js
@@ -7,6 +7,8 @@ function App() {
   const [numCorrect, setNumCorrect] = useState(0)
   const [winning, setWinning] = useState(true)
 
+  // fetch('/new-table').then((res) => res.json()).then((data) => console.log(data))
+
   useEffect(() => {
     fetchKantoPokemon()
     // async function handleAsync() {
@@ -48,13 +50,14 @@ function App() {
 
   function fetchKantoPokemon() {
     let pokeId = Math.floor(Math.random() * 100)
+    // let pokeId = Math.floor(3)
     fetch(`https://pokeapi.co/api/v2/pokemon/${pokeId}`)
       .then((response) => {
-        console.log(response)
+        // console.log(response)
         return response.json()
       })
       .then(function (pokeData) {
-        console.log(pokeData)
+        // console.log(pokeData)
         setPokemon(pokeData)
         // allpokemon.results.forEach(function (pokemon) {
         //   fetchPokemonData(pokemon)
@@ -90,7 +93,6 @@ function App() {
         canvas.height = img.height / 4
         canvas.width = img.width / 4
         ctx.drawImage(img, 0, 0, canvas.width, canvas.height)
-        console.log('image draw')
         // pixelate()
         //dynamically adjust canvas size to the size of the uploaded image
         canvas.height = img.height
@@ -117,18 +119,149 @@ function App() {
   }
 
   const buildRgb = (imageData) => {
-    const rgbValues = [];
+    const rgbValues = []
     for (let i = 0; i < imageData.length; i += 4) {
-      const rgb = {
-        r: imageData[i],
-        g: imageData[i + 1],
-        b: imageData[i + 2],
-      };
-      rgbValues.push(rgb);
+      if (imageData[i + 3] > 0) {
+        const rgb = {
+          r: imageData[i],
+          g: imageData[i + 1],
+          b: imageData[i + 2]
+        }
+        rgbValues.push(rgb)
+      }
     }
-    return rgbValues;
-  };
+    return rgbValues
+  }
 
+  const findBiggestColorRange = (rgbValues) => {
+    let rMin = Number.MAX_VALUE //start min at greatest possible number
+    let gMin = Number.MAX_VALUE
+    let bMin = Number.MAX_VALUE
+
+    let rMax = Number.MIN_VALUE //start max at tiniest possible number
+    let gMax = Number.MIN_VALUE
+    let bMax = Number.MIN_VALUE
+
+    rgbValues.forEach((pixel) => {
+      rMin = Math.min(rMin, pixel.r)
+      gMin = Math.min(gMin, pixel.g)
+      bMin = Math.min(bMin, pixel.b)
+
+      rMax = Math.max(rMax, pixel.r)
+      gMax = Math.max(gMax, pixel.g)
+      bMax = Math.max(bMax, pixel.b)
+    })
+
+    const rRange = rMax - rMin
+    const gRange = gMax - gMin
+    const bRange = bMax - bMin
+
+    const biggestRange = Math.max(rRange, gRange, bRange)
+    if (biggestRange === rRange) {
+      return 'r'
+    } else if (biggestRange === gRange) {
+      return 'g'
+    } else {
+      return 'b'
+    }
+  }
+
+  const quantization = (rgbValues, depth) => {
+    //base code
+    const MAX_DEPTH = 2
+    if (depth === MAX_DEPTH || rgbValues.length === 0) {
+      const color = rgbValues.reduce(
+        (prev, curr) => {
+          prev.r += curr.r
+          prev.g += curr.g
+          prev.b += curr.b
+
+          return prev
+        },
+        {
+          r: 0,
+          g: 0,
+          b: 0
+        }
+      )
+
+      color.r = Math.round(color.r / rgbValues.length)
+      color.g = Math.round(color.g / rgbValues.length)
+      color.b = Math.round(color.b / rgbValues.length)
+      return [color]
+    }
+
+    //recursion code
+    const componentToSortBy = findBiggestColorRange(rgbValues)
+    rgbValues.sort((p1, p2) => {
+      return p1[componentToSortBy] - p2[componentToSortBy]
+    })
+
+    let avg =
+      rgbValues.reduce((a, b) => a + b[componentToSortBy], 0) / rgbValues.length
+    let index = rgbValues.findIndex((value) => value[componentToSortBy] >= avg)
+    // console.log(avg, index)
+    // const mid = rgbValues.length / 2
+    return [
+      ...quantization(rgbValues.slice(0, index), depth + 1),
+      ...quantization(rgbValues.slice(index + 1), depth + 1)
+    ]
+    // return [
+    //   ...quantization(rgbValues.slice(0, mid), depth + 1),
+    //   ...quantization(rgbValues.slice(mid + 1), depth + 1)
+    // ]
+  }
+  const [rgbPalette, setRgbPalette] = useState(null)
+  const [hsvPalette, setHsvPalette] = useState(null);
+  const [type, setType] = useState(null);
+
+  const rgbToHsv = (item) => {
+    let { r, g, b } = item
+    // R, G, B values are divided by 255
+    // to change the range from 0..255 to 0..1
+    r = r / 255.0
+    g = g / 255.0
+    b = b / 255.0
+
+    // h, s, v = hue, saturation, value
+    var cmax = Math.max(r, Math.max(g, b)) // maximum of r, g, b
+    var cmin = Math.min(r, Math.min(g, b)) // minimum of r, g, b
+    var diff = cmax - cmin // diff of cmax and cmin.
+    var h = -1,
+      s = -1
+
+    // if cmax and cmin are equal then h = 0
+    if (cmax == cmin) h = 0
+    // if cmax equal r then compute h
+    else if (cmax == r) h = (60 * ((g - b) / diff) + 360) % 360
+    // if cmax equal g then compute h
+    else if (cmax == g) h = (60 * ((b - r) / diff) + 120) % 360
+    // if cmax equal b then compute h
+    else if (cmax == b) h = (60 * ((r - g) / diff) + 240) % 360
+
+    // if cmax equal zero
+    if (cmax == 0) s = 0
+    else s = (diff / cmax) * 100
+
+    // compute v
+    var v = cmax * 100
+    return { h: Math.round(h), s: Math.round(s), v: Math.round(v) }
+  }
+
+  const getType = (values) => {
+    let hMin = Number.MAX_VALUE //start min at greatest possible number
+
+    let hMax = Number.MIN_VALUE //start max at tiniest possible number
+
+
+    values.forEach((obj) => {
+      hMin = Math.min(hMin, obj.h)
+      hMax = Math.max(hMax, obj.h)
+
+    })
+    const range = hMax - hMin
+    return range
+  }
   const getPalette = () => {
     // const imgFile = document.getElementById('imgfile')
     const image = new Image()
@@ -146,11 +279,29 @@ function App() {
       ctx.drawImage(image, 0, 0)
 
       const imageData = ctx.getImageData(0, 0, canvas.width, canvas.height)
-      console.log(imageData)
       let rgbValues = buildRgb(imageData.data)
-      console.log(rgbValues)
+      let newValues = quantization(rgbValues, 0)
+      setRgbPalette(newValues)
+      let tmp = []
+      newValues.forEach((color) => {
+       let converted = rgbToHsv(color)
+       tmp.push(converted)
+      })
+      setHsvPalette(tmp)
+      let range = getType(tmp)
+      if (range > 160) {
+        setType('complementary')
+      } else if (range > 100) {
+        setType('analogous')
+      } else if (range > 10) {
+        setType('monochrome')
+      }
     }
     // }
+  }
+
+  const sendIt = () => {
+    console.log(pokemon.id, pokemon.name, rgbPalette, hsvPalette, type)
   }
 
   useEffect(() => {
@@ -170,13 +321,13 @@ function App() {
           <>
             <div>
               {/* <p>{pokemon.name}</p> */}
-              <img
+              {/* <img
                 id='imgfile'
                 src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemon.id}.png`}
                 alt='Girl in a jacket'
                 width='100'
                 height='100'
-              ></img>
+              ></img> */}
             </div>
             <canvas
               id='canvas'
@@ -184,6 +335,22 @@ function App() {
               height='200'
               style={{ border: 'solid 1px black' }}
             ></canvas>
+            {rgbPalette ? (
+              <div style={{ display: 'flex' }}>
+                {rgbPalette.map((color, index) => (
+                  <div
+                    key={index}
+                    style={{
+                      width: '100px',
+                      height: '100px',
+                      backgroundColor: `rgb(${color.r}, ${color.g}, ${color.b})`
+                    }}
+                  ></div>
+                ))}
+              </div>
+            ) : (
+              <></>
+            )}
             <div>
               <label htmlFor='pokeName'>Name</label>
               <input
@@ -202,6 +369,7 @@ function App() {
             <button onClick={() => handleReset()}>Try Again</button>
           </>
         )}
+        <button onClick={() => sendIt()}>SEND IT!</button>
         {/* <p>{!data ? 'loading...' : data}</p> */}
       </header>
     </div>
--- server/index.js
+++ server/index.js
@@ -1,23 +1,70 @@
-const path = require('path');
-const express = require("express");
+const path = require('path')
+const express = require('express')
+const mysql = require('mysql')
 //Heroku sets this value when deployed, otherwise runs on port 3001
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3001
 
-const app = express();
+const app = express()
+
+// var connection = mysql.createConnection({
+//   host: 'poke-palette-db.cbygkpyx6qob.us-east-2.rds.amazonaws.com',
+//   user: 'admin',
+//   password: 'GreenD0g!',
+//   port: 3306,
+//   database: 'test_schema'
+// })
+
+// app.get('/new-table', (req, res) => {
+//   connection.connect(function(err) {
+//     connection.query(`SELECT * FROM new_table`, function(err, result, fields) {
+//       if (err) res.send(err);
+//       if (result) res.json({data: result});
+      
+//     })
+//   })
+// })
+
+
+// connection.connect(function(err) {
+//   if (err) {
+//     console.log('Database connection failed:'+ err.stack)
+//     return
+//   }
+//   console.log('Connected to database.')
+// })
+// connection.end()
+
+// connection.connect(function(err) {
+//   if (err) throw err;
+
+//   connection.query('CREATE DATABASE IF NOT EXISTS main;');
+//   connection.query('USE main;');
+//   connection.query('CREATE TABLE IF NOT EXISTS users(id int NOT NULL AUTO_INCREMENT, username varchar(30), email varchar(255), age int, PRIMARY KEY(id));', function(error, result, fields) {
+//       console.log(result);
+//   });
+//   connection.end();
+// });
 
 // Have Node serve the files for our built React app
-app.use(express.static(path.resolve(__dirname, '../client/build')));
+app.use(express.static(path.resolve(__dirname, '../client/build')))
 
 // Handle GET requests to /api route
-app.get("/api", (req, res) => {
-    res.json({ message: "Hello from server!" });
-  });
+app.get('/api', (req, res) => {
+  res.json({ message: 'Hello from server!' })
+})
 
-  // All other GET requests not handled before will return our React app
+// All other GET requests not handled before will return our React app
 app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
-});
-  
-  app.listen(PORT, () => {
-    console.log(`Server listening on ${PORT}`);
-  });
+  res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'))
+})
+
+app.listen(PORT, () => {
+  console.log(`Server listening on ${PORT}`)
+})
+
+process.on( 'SIGINT', function() {
+  console.log( "\nGracefully shutting down from SIGINT (Ctrl-C)" );
+  connection.end();
+  // some other closing procedures go here
+  process.exit( );
+})
